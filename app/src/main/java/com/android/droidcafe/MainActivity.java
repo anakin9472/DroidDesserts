b80a5c69d1751f1a826473e58e3df6d7790caba3
anakin9472 <nguyenphuocdang1234@gmail.com>
2022-04-12 14:45:29 +0700

    Update function
    add Button and function

--- app/src/main/java/com/android/droidcafe/MainActivity.java
+++ app/src/main/java/com/android/droidcafe/MainActivity.java
@@ -2,12 +2,14 @@ package com.android.droidcafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public String mOrderMessage="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,15 +24,32 @@ public class MainActivity extends AppCompatActivity {
     public void orderDonuts(View view)
     {
         displayToast(getString(R.string.donut_order_message));
+        mOrderMessage = getString(R.string.donut_order_message);
     }
 
     public void orderIceCream(View view)
     {
         displayToast(getString(R.string.ice_cream_order_message));
+        mOrderMessage = getString(R.string.ice_cream_order_message);
     }
 
     public void orderFroyo(View view)
     {
         displayToast(getString(R.string.froyo_order_message));
+        mOrderMessage = getString(R.string.froyo_order_message);
+    }
+
+    public void onClick(View view)
+    {
+        if (mOrderMessage == "")
+        {
+            Toast.makeText(getApplicationContext(),"Please order!",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+            intent.putExtra("yourOrder", mOrderMessage);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/android/droidcafe/OrderActivity.java
@@ -0,0 +1,25 @@
+package com.android.droidcafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("yourOrder");
+        TextView textView = findViewById(R.id.textViewMessage);
+        textView.setText(message);
+    }
+
+
+
+}
